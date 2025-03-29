0a5d0157ebd63290d6746e0e5deeb2be6b2cc7ff
Angelina Bezik <angelina1337bz@gmail.com>
2025-03-29 23:48:31 +0300

    feat: create 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Routes } from '@/types/routes';
+import { Result, Button } from 'antd';
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link href={Routes.MAIN} passHref>
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
+  );
+};
+
+export default NotFoundPage;
