9b4f798e091af631a7d26d4c15e3449ab6ed7a2e
Angelina Bezik <angelina1337bz@gmail.com>
2025-03-30 16:02:20 +0300

    refactor: update with types

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,17 @@
 import { Routes } from '@/types/routes';
 import { Result, Button } from 'antd';
 import Link from 'next/link';
+import { ERROR_MAP, Status } from '@/types';
 
 const NotFoundPage = () => {
+  const status = Status[404];
+  const meta = ERROR_MAP[status];
+
   return (
     <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
+      status={status}
+      title={meta.title}
+      subTitle={meta.subTitle}
       extra={
         <Link href={Routes.MAIN} passHref>
           <Button type="primary">Back Home</Button>
