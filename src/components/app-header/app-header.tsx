d31ca1029e4db0a77502a19f90a5d3590ff4ad63
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-03-31 21:24:48 +0300

    feat: add header elements in app-header.tsx

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Button, Layout, Space } from 'antd';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import LocaleSwitcher from '../locale-switcher/locale-switcher';
+import styles from './app-header.module.css';
+import { Routes } from '@/types/routes';
+
+const { Header } = Layout;
+
+const AppHeader = (): React.JSX.Element => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    setIsSticky(false);
+  }, []);
+
+  return (
+    <Header className={`${styles.header} ${isSticky ? styles.sticky : ''}`}>
+      <Link className={styles.logo} href={Routes.MAIN}>
+        REST Client
+      </Link>
+
+      <LocaleSwitcher />
+
+      <Space
+        className={styles['header-container']}
+        size="middle"
+        align="center"
+      >
+        <Link href="/signin">
+          <Button type="link" color="blue" variant="filled">
+            Sign In
+          </Button>
+        </Link>
+
+        <Link href="/signup">
+          <Button type="primary">Sign Up</Button>
+        </Link>
+      </Space>
+    </Header>
+  );
+};
+
+export default AppHeader;
