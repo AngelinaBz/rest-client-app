7675f39469a4a8adffeadd87da165ec4f7661209
Angelina Bezik <angelina1337bz@gmail.com>
2025-04-04 12:35:50 +0300

    feat: update styles

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { Typography } from 'antd';
+import { Button, Typography } from 'antd';
 import Team from '../team/team';
 import styles from './main-content.module.css';
+import { Routes } from '@/types/routes';
 
 const { Title, Paragraph } = Typography;
 
@@ -11,9 +12,17 @@ const MainContent = () => {
   const t = useTranslations('MainPage');
 
   return (
-    <div className={styles.main}>
-      <Title>Restful Client</Title>
-      <Paragraph>{t('description')}</Paragraph>
+    <div className={styles['main-container']}>
+      <section className={styles['main-section']}>
+        <div className={styles['title-container']}>
+          <Title>Restful Client</Title>
+          <Paragraph>{t('description')}</Paragraph>
+          <Button type="primary" href={Routes.SIGN_IN} target="_blank">
+            {t('button')}
+          </Button>
+        </div>
+        <img alt="rest-img" src="main.png" className={styles['main-img']} />
+      </section>
       <Title level={2}>{t('teamTitle')}</Title>
       <Paragraph>{t('teamDescription')}</Paragraph>
       <Team />
