6337113dd76b784affca155c523a14e77379aaa7
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-04-04 20:16:52 +0300

    feat: add restful client page layout storing response to localstorage

---
+++ src/components/submit-button/submit-button.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'antd';
+import { useTranslations } from 'use-intl';
+
+interface SubmitButtonProps {
+  onClick: () => void;
+}
+
+const SubmitButton = ({ onClick }: SubmitButtonProps) => {
+  const t = useTranslations('RestfulClient');
+
+  return (
+    <Button type="primary" onClick={onClick} style={{ width: '100%' }}>
+      {t('sendRequest')}
+    </Button>
+  );
+};
+
+export default SubmitButton;
