c15eebb03857155f1d96df8b0950a48cf6147ea9
pambaka <pambaka@ya.ru>
2025-04-14 14:21:00 +0200

    fix: fix metadata translation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,7 @@ import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { Inter } from 'next/font/google';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { routing } from '@/i18n/routing';
-import { RootLayoutProps } from '@/types';
+import { Params, RootLayoutProps } from '@/types';
 import { AppFooter } from '@/components/app-footer';
 import AntdConfigProvider from '@/providers/antd-config-provider';
 import UserProvider from '@/providers/user-provider';
@@ -11,15 +11,32 @@ import '@ant-design/v5-patch-for-react-19';
 import '../global.css';
 import { AppHeader } from '@/components/app-header';
 import NotFoundPage from './[not-found]/page';
+import { DEFAULT_LOCALE } from '@/utils/constants';
+import { getTranslations } from 'next-intl/server';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'REST Client App',
-  description: 'A modern RESTful API client',
-  icons: {
-    icon: 'icon.png',
-  },
+type MetadataProps = { params: Promise<Params> };
+
+export const generateMetadata = async ({
+  params,
+}: MetadataProps): Promise<Metadata> => {
+  const { locale } = await params;
+  const correctLocale = hasLocale(routing.locales, locale)
+    ? locale
+    : DEFAULT_LOCALE;
+  const t = await getTranslations({
+    locale: correctLocale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    icons: {
+      icon: 'icon.png',
+    },
+  };
 };
 
 const RootLayout = async ({ children, params }: RootLayoutProps) => {
