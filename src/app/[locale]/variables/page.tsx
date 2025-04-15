519cad71d5b889963cb5504cc09bddbd8f27eea8
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2025-04-15 14:49:34 +0300

    feat: save variables to local storage (#78)
    * feat: create local storage hook
    
    * refactor: update hook
    
    * feat: add a check for empty fields
    
    * feat: add unique key check
    
    * fix: change key func
    
    * fix: update message
    
    * refactor: update editor with dynamic
    
    * refactor: add usecallback and constant for unique key

--- src/app/[locale]/variables/page.tsx
+++ src/app/[locale]/variables/page.tsx
@@ -2,9 +2,14 @@
 
 import { ReactNode } from 'react';
 import { useTranslations } from 'use-intl';
-import VariablesEditor from '@/components/variables-editor/variables-editor';
-import { Typography } from 'antd';
+import { Spin, Typography } from 'antd';
 import { PageWrapper } from '@/components/page-wrapper';
+import dynamic from 'next/dynamic';
+
+const VariablesEditor = dynamic(() => import('@/components/variables-editor'), {
+  ssr: false,
+  loading: () => <Spin></Spin>,
+});
 
 const { Title } = Typography;
 
--- src/components/variables-editor/variables-editor.tsx
+++ src/components/variables-editor/variables-editor.tsx
@@ -1,20 +1,37 @@
 'use client';
 
 import { DeleteOutlined } from '@ant-design/icons';
-import { Button, Flex, Tooltip, Input, Empty } from 'antd';
+import { Button, Flex, Tooltip, Input, Empty, Form } from 'antd';
 import { useTranslations } from 'use-intl';
-import useEditorItems from '@/hooks/use-editor-items';
+import useVariablesLocalStorage from '@/hooks/use-variables-localstorage';
+import { useCallback } from 'react';
 
 const VariablesEditor = () => {
   const t = useTranslations('Variables');
 
-  const [variables, setVariables] = useEditorItems();
+  const [variables, setVariables] = useVariablesLocalStorage();
 
-  const updateVariables = (index: number, key: string, value: string) =>
-    setVariables({
-      type: 'update',
-      payload: { index, key, value },
-    });
+  const isKeyUnique = useCallback(
+    (key: string, index: number) => {
+      if (key.trim() === '') {
+        return true;
+      }
+      return !variables.some(
+        (variable, i) => variable.key === key && i !== index
+      );
+    },
+    [variables]
+  );
+
+  const updateVariables = useCallback(
+    (index: number, key: string, value: string) => {
+      setVariables({
+        type: 'update',
+        payload: { index, key, value },
+      });
+    },
+    [setVariables]
+  );
 
   return (
     <Flex vertical style={{ padding: '10px' }} gap="small">
@@ -27,31 +44,45 @@ const VariablesEditor = () => {
       {variables.length === 0 ? (
         <Empty description={t('message')} />
       ) : (
-        variables.map((variable, index) => (
-          <Flex key={index} gap="small">
-            <Input
-              placeholder={t('variableKeyPlaceholder')}
-              value={variable.key}
-              onChange={(e) =>
-                updateVariables(index, e.target.value, variable.value)
-              }
-            />
-            <Input
-              placeholder={t('variableValuePlaceholder')}
-              value={variable.value}
-              onChange={(e) =>
-                updateVariables(index, variable.key, e.target.value)
-              }
-            />
-            <Tooltip title={t('deleteVariable')}>
-              <DeleteOutlined
-                onClick={() =>
-                  setVariables({ type: 'remove', payload: { index } })
-                }
-              />
-            </Tooltip>
-          </Flex>
-        ))
+        variables.map((variable, index) => {
+          const isUnique = isKeyUnique(variable.key, index);
+
+          return (
+            <Flex key={index} gap="small">
+              <Form.Item
+                validateStatus={!isUnique ? 'error' : ''}
+                help={!isUnique ? t('keyUniqueError') : ''}
+                style={{ marginBottom: 0, flex: 1 }}
+              >
+                <Input
+                  placeholder={t('variableKeyPlaceholder')}
+                  value={variable.key}
+                  onChange={(e) =>
+                    updateVariables(index, e.target.value, variable.value)
+                  }
+                />
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 0, flex: 1 }}>
+                <Input
+                  placeholder={t('variableValuePlaceholder')}
+                  value={variable.value}
+                  onChange={(e) =>
+                    updateVariables(index, variable.key, e.target.value)
+                  }
+                />
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 0 }}>
+                <Tooltip title={t('deleteVariable')}>
+                  <DeleteOutlined
+                    onClick={() =>
+                      setVariables({ type: 'remove', payload: { index } })
+                    }
+                  />
+                </Tooltip>
+              </Form.Item>
+            </Flex>
+          );
+        })
       )}
     </Flex>
   );
