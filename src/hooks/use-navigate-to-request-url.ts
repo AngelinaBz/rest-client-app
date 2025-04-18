8fafe9bd2f8586d1a7aee04160b7a4d31e4c6b9c
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-04-18 23:35:35 +0300

    fix: add setcookie in use-navigate-to-url

--- src/hooks/use-navigate-to-request-url.ts
+++ src/hooks/use-navigate-to-request-url.ts
@@ -7,18 +7,25 @@ import useVariablesLocalStorage from './use-variables-localstorage';
 import { useHistoryLocalStorage } from './use-history-localstorage';
 import { addRequestToHistory } from '@/utils/history-utils';
 import { buildRequestPath } from '@/utils/request-path';
+import { setCookie } from 'cookies-next';
 
 export const useNavigateToRequestURL = () => {
   const router = useRouter();
   const [variables] = useVariablesLocalStorage();
   const [, setHistory] = useHistoryLocalStorage();
 
-  return (request: RequestParams) => {
-    const finalRequest = prepareRequestData(request, variables);
+  return (originalRequest: RequestParams) => {
+    const finalRequest = prepareRequestData(originalRequest, variables);
 
     addRequestToHistory(finalRequest, variables, setHistory);
 
-    const path = buildRequestPath(request);
+    variables.forEach(({ key, value }) => {
+      if (key && value) {
+        setCookie(`rest_var_${key}`, value);
+      }
+    });
+
+    const path = buildRequestPath(originalRequest);
 
     router.replace(path, { scroll: false });
   };
